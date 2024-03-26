0c0d533591ac845f9e8e5386e84dd44421062ffe
Krille, Jan-Victor <jan-victor.krille@vector.com>
2024-03-26 12:34:42 +0100

    Use ngStyle

--- src/app/renderer/renderer.component.ts
+++ src/app/renderer/renderer.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ScreenRenderData } from '../interface/engine.interface';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'ht3d-renderer',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './renderer.component.html',
   styleUrl: './renderer.component.less'
 })
@@ -12,4 +13,9 @@ export class RendererComponent {
   @Input() width?: number = 400;
   @Input() height?: number = 300;
   @Input() vlines?: ScreenRenderData = [];
+
+  ceilingColorFar = 'rgba(0,212,255,1)';
+  ceilingColorNear = 'rgba(9,9,121,1)';
+  floorColorFar = 'rgba(8,66,7,1)';
+  floorColorNear = 'rgba(73,121,9,1)';
 }
